c8d11457c2af0c7b4686b863dfb7b8d4cd023a2d
tjqiulu <tjqiulu@hotmail.com>
2016-12-19 15:34:46 +0800

    update config server

--- dev/CloudConfigServer/src/main/java/com/gemalto/CloudConfigServerApplication.java
+++ dev/CloudConfigServer/src/main/java/com/gemalto/CloudConfigServerApplication.java
@@ -1,12 +1,16 @@
 package com.gemalto;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 
 
 @SpringBootApplication
 @EnableConfigServer
+@Configuration
+@EnableAutoConfiguration
 public class CloudConfigServerApplication {
 
 	public static void main(String[] args) {
