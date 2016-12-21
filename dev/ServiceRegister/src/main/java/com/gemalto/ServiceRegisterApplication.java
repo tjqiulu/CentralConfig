a11a6616de48a8673cd10e9855d455dfa28bde84
tjqiulu <tjqiulu@hotmail.com>
2016-12-21 09:46:16 +0800

    add new service register

--- dev/ServiceRegister/src/main/java/com/gemalto/ServiceRegisterApplication.java
+++ dev/ServiceRegister/src/main/java/com/gemalto/ServiceRegisterApplication.java
@@ -2,14 +2,13 @@ package com.gemalto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class ClientDemoApplication {
+@EnableEurekaServer
+public class ServiceRegisterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ClientDemoApplication.class, args);
+		SpringApplication.run(ServiceRegisterApplication.class, args);
 	}
-	
 }
